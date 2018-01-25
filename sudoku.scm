58f3b3ef5ae475a68a832a90f831d4669fe968b6
Nick Barth <nbarth@goauto.ca>
2018-01-24 22:05:14 -0700

    cleaned up project

--- sudoku.scm
+++ sudoku.scm
@@ -1,3 +1,4 @@
+(import chicken scheme)
 (use srfi-1)
 
 ; make-board :-> [][]int
@@ -26,7 +27,7 @@
 
 ; test : set-row
 (define (test-set-row)
-  (unless (equal? (cadr (set-row (make-board) 1 '(5))) '(5))
+  (unless (equal? (cadr (set-row '((0 0 0) (0 0 0)) 1 '(5))) '(5))
     (print "ERROR - row not set")))
 (test-set-row)
 
@@ -36,8 +37,8 @@
 
 ; test : get-row
 (define (test-get-row)
-  (let ((board (set-row (make-board) 2 '(1 1 1))))
-    (unless (equal? (get-row board 2) '(1 1 1))
+  (let ((board (set-row '((0 0 0) (0 0 0)) 1 '(1 1 1))))
+    (unless (equal? (get-row board 1) '(1 1 1))
       (print "ERROR - get-row incorrect"))))
 (test-get-row)
 
@@ -60,8 +61,8 @@
 
 ; test : get-column
 (define (test-get-column)
-  (let ((board (set-row (make-board) 1 '(1 1))))
-    (unless (equal? (get-column board 1) '(0 1 0 0 0 0 0 0 0))
+  (let ((board (set-row '((0 0 0)(0 0 0)(0 0 0)) 1 '(1 1 1))))
+    (unless (equal? (get-column board 1) '(0 1 0))
       (print "ERROR - get-column incorrect"))))
 (test-get-column)
 
@@ -70,12 +71,11 @@
   (let ((section (take (drop board (* y 3)) 3)))
     (fold (lambda (col r)
       (append r (take (drop col (* x 3)) 3))) '() section)))
-(get-quad (make-board) 2 2)
 
 ; test : get-quad
 (define (test-get-quad)
-  (let ((board (set-row (make-board) 3 '(0 0 0 1 2 3))))
-    (unless (equal? (get-quad board 1 1) '(1 2 3 0 0 0 0 0 0))
+  (let ((board '((1 2 3 0 0 0 0 0 0) (4 5 6 0 0 0 0 0 0 0 0 0) (7 8 9 0 0 0 0 0 0 0 0 0))))
+    (unless (equal? (get-quad board 0 0) '(1 2 3 4 5 6 7 8 9))
       (print "ERROR - get-quad incorrect"))))
 (test-get-quad)
 
@@ -101,15 +101,27 @@
 
 ; test : avaliable-at-position
 (define (test-available-at-position)
-  (let ((board (set-row (make-board) 0 '(0 1 2 3 4 6 7 8 9))))
+  (let ((board '((0 1 2 3 4 6 7 8 9) (0 0 0 0 0 0 0 0 0) (0 0 0 0 0 0 0 0 0))))
     (unless (equal? (avaliable-at-position board 0 0) '(5))
       (print "ERROR - get-quad incorrect"))))
 (test-available-at-position)
 
+; has-1-9? : []int -> bool
+(define (has-1-9? nums)
+  (let ((1-9 '(1 2 3 4 5 6 7 8 9)))
+    (equal? 1-9 (fold (lambda (n r)
+      (if (member n nums) (append r (list n)) r)) '() 1-9))))
+
+; test : has-1-9?
+(define (test-has-1-9)
+  (unless (has-1-9? '(2 1 3 4 5 6 7 8 9))
+    (print "ERROR - has-1-9? incorrect")))
+(test-has-1-9)
+
 ; rows-solved? : [][]int -> bool
 (define (rows-solved? board)
   (fold (lambda (row r)
-    (and (equal? (sort row <) '(1 2 3 4 5 6 7 8 9)) r))
+    (and (has-1-9? row) r))
       #t board))
 
 ; test : rows-solved?
@@ -130,7 +142,7 @@
 ; cols-solved? : [][]int -> bool
 (define (cols-solved? board)
   (fold (lambda (col r)
-    (and (equal? (sort (get-column board col) <) '(1 2 3 4 5 6 7 8 9)) r))
+    (and (has-1-9? (get-column board col)) r))
       #t '(0 1 2 3 4 5 6 7 8)))
 
 ; test : cols-solved?
@@ -151,7 +163,7 @@
 ; quads-solved? : [][]int -> bool
 (define (quads-solved? board)
   (fold (lambda (quad r)
-    (and (equal? (sort (get-quad board (car quad) (cadr quad)) <) '(1 2 3 4 5 6 7 8 9)) r))
+    (and (has-1-9? (get-quad board (car quad) (cadr quad))) r))
       #t '((0 0) (0 1) (0 2) (1 0) (1 1) (1 2) (2 0) (2 1) (2 2))))
 
 ; test : quads-solved?
@@ -217,8 +229,12 @@
       (let ((pos (find-space board)))
         (fold (lambda (n r)
           (let ((test-board (set-position board (car pos) (cadr pos) n)))
-            (or (solve test-board) r)))
-          #f (avaliable-at-position board (car pos) (cadr pos)))))))
+            (or (solve test-board) r))) #f (avaliable-at-position board (car pos) (cadr pos)))))))
+
+
+(define (pp board)
+  (for-each (lambda (row)
+    (print row)) board))
 
 (define (main)
   (let ((board
@@ -235,3 +251,19 @@
   (print "-----")
   (pp (solve board))))
 (main)
+
+
+; (with-input-from-file path (lambda () (let loop ((count 0)) (if (eof-object? (read-line)) count (loop (+ count 1)))))))
+; (with-input-from-file path (lambda () (let loop ((count 0) (c (read-char))) (if (eof-object? c) count (loop (if (char=? c #\newline) (+ count 1) count) (read-char))))))
+; (with-input-from-file "./sudoku.txt" (lambda () (let loop ((count 0)) (if (eof-object? (read-line)) count (loop (+ count 1)))))))
+; (let ((data (string-delete #\newline
+;             (string-delete #\space
+;        (read-all "sudoku.txt")))))
+;  data)
+; (string-length "123")
+; (string-delete #\space  "a d f a d f \n")
+; (string-delete #\newline  "a d f a d f \n")
+; (string-delete #\0 "1101")
+; (open-output-file "sudoku.txt")
+; (substring "1 2 4" 1)
+; (string->number "15")
